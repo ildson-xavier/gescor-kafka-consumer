1c9561476b7889c0b57142fe12c4f3c79bb5800d
Ildson <ildson.xavier@gmail.com>
2019-09-25 18:25:46 -0300

    Consumer

---
+++ src/main/java/br/com/gescor/gescorkafkaconsumer/engine/Consumer.java
@@ -0,0 +1,24 @@
+package br.com.gescor.gescorkafkaconsumer.engine;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class Consumer {
+
+	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+    @Autowired
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @KafkaListener(topics = "users", groupId = "group_id")
+    public void consume(String message) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+}
